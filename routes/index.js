8e42ee793683b8c973d449612e31597454bc2116
Markus Springer <markus.springer1994@googlemail.com>
2016-03-01 12:51:06 +0100

    Updated index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var http = require('http');
+var https = require('https');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/:user', function(req, res, next) {
+
   res.render('index', { title: req.params.user });
 
 });
