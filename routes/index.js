0b2afcbb4503c588c3bf2ed3e2e75fee95ffc76f
Markus Springer <markus.springer1994@googlemail.com>
2016-03-01 10:42:39 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:user', function(req, res, next) {
+  res.render('index', { title: req.params.user });
+
+});
+
+module.exports = router;
